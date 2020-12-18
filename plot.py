67190381b2e0f5f4b7bc16ac744f5669eac546e6
Fabian Peter Hammerle <fabian@hammerle.me>
2020-12-18 18:12:36 +0100

    output: add percent sign

--- plot.py
+++ plot.py
@@ -124,18 +124,18 @@ def _main():
         displayed_relative_humidity = displayed_values.get(recording_path.name, {}).get(
             "relative_humidity"
         )
-        relative_humidity, = numpy.packbits(
-            messages_data_bits[0, 22:30], bitorder="big"
+        relative_humidity_percent, = (
+            numpy.packbits(messages_data_bits[0, 22:30], bitorder="big") + 16
         )
         assert not displayed_relative_humidity or (
             int(displayed_relative_humidity * 100) & 0b1111
-        ) == (relative_humidity & 0b1111)
+        ) == (relative_humidity_percent & 0b1111)
         print(
             recording_path.name.split(".", maxsplit=1)[0],
             "".join(map(str, map(int, messages_data_bits[0, :10]))),
             "".join(map(str, map(int, messages_data_bits[0, 10:14]))),
             "".join(map(str, map(int, messages_data_bits[0, 14:22]))),  # temp?
-            relative_humidity + 16,  # humidity
+            "{:.0f}%".format(relative_humidity_percent),
             "".join(map(str, map(int, messages_data_bits[0, 30:35]))),
             "".join(map(str, map(int, messages_data_bits[0, 35:]))),  # checksum?
             displayed_temperature_degrees_celsius,
